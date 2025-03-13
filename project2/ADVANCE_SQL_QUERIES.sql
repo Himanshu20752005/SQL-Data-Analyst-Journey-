8f3607553f528c83022ed6a8198eb6f5c2f84faf
Himanshu Pandey <114969802+Himanshu20752005@users.noreply.github.com>
2025-03-13 23:07:35 +0530

    Update ADVANCE_SQL_QUERIES.sql
    updating 1st query

--- project2/ADVANCE_SQL_QUERIES.sql
+++ project2/ADVANCE_SQL_QUERIES.sql
@@ -6,9 +6,23 @@
    Display the member's_id, member's name, book title, issue date, and days overdue. */
 
 SELECT 
-    issued_member_id,
-    member_name,
-    book_title,
-    issued_date,
+    iss.issued_member_id,
+    m.member_name,
+    b.book_title,
+    iss.issued_date,
+	current_date - iss.issued_date as overdue_date 
+from issued_status as iss 
+join members as m
+on iss.issued_member_id = m.member_id
+join books as b
+on iss.issued_book_isbn = b.isbn
+left join return_status as rs
+on rs.issued_id = iss.issued_id
+where rs.return_date is null 
+     and (current_date - iss.issued_date) > 30
+order by 1;
 
-	current_date - issued_date
+
+
+
+	
